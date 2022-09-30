66394d74ed736ae76a778d066949451a880e4764
Aarush Verma <aarushverma@Aarushs-MBP.dhost.uta.edu>
2022-09-30 01:49:49 -0500

    arrays1

--- Arrays/BinarySearch/BinarySearchIterative.java
+++ Arrays/BinarySearch/BinarySearchIterative.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class BinarySearchIterative {
+public class BinarySearchIterative {
+    public int search(int[] nums, int target) {
+
+        //Arrays.sort(nums);
+
+        int low = 0;
+        int high  = nums.length-1;
+
+        while(low<=high){
+
+            int mid = ((high + low) / 2);
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else if(nums[mid] > target){
+                high  = mid -1;
+            }
+        }
+
+        return -1;
+
+
+    }
 }
--- Arrays/BinarySearch/BinarySearchRecursive.java
+++ Arrays/BinarySearch/BinarySearchRecursive.java
@@ -1,4 +1,38 @@
-public class BinarySearch {
+public class BinarySearchRecursive {
 
-    
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        return search2(nums,low,high,target);
+
+    }
+    //return -1;
+    public static int search2(int[] nums,int low, int high, int target){
+
+
+
+        if(low > high){
+            return -1;
+        }
+
+        int mid = low + (high - low)/2;
+
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[mid] > target){
+            return search2(nums,low,mid-1,target);
+        }
+
+        else if(nums[mid] < target){
+
+            return search2(nums,mid+1,high,target);
+
+
+        }
+
+        return -1;
+    }
+
+
 }
